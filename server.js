a6cd03d57c309a0bbe0b6c03bfa87ad005cdefcd
Jared Suasin <jsuasin@ucsd.edu>
2020-10-12 12:51:31 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+
 const port = 3000;
 //Loads the handlebars module
 const handlebars = require('express-handlebars');
 //Sets our app to use the handlebars engine
 app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
 //Sets handlebars configurations (we will go through them later on)
 app.engine('handlebars', handlebars({
     layoutsDir: __dirname + '/views/layouts',
@@ -15,4 +20,7 @@ app.get('/', (req, res) => {
 res.render('placeholder', {layout: 'index'});
 });
 
-app.listen(port, () => console.log(`App listening to port ${port}`));
+//app.listen(port, () => console.log(`App listening to port ${port}`));
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+  });

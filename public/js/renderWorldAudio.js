50d720fc3a836337be2e029b7359031f9938e61b
Jared Suasin <jsuasin@ucsd.edu>
2020-10-12 19:48:50 -0700

    Add artist tag, link to SC

--- public/js/renderWorldAudio.js
+++ public/js/renderWorldAudio.js
@@ -5,8 +5,9 @@ function play() {
     audio.play();
 
     document.getElementById('scrim').classList.add('hidden');
-    document.getElementById('buttonContainer').classList.add('hidden');
+    document.getElementById('modalContainer').classList.add('hidden');
     document.getElementById('enterButton').classList.add('hidden');
+    document.getElementById('enterDetails').classList.add('hidden');
 
     //document.getElementById('soundcloudButton').classList.add('show');
 }
